c6b6f546155bfe04b138b51432d56d14a1093328
theoyoth <theobantang1@gmail.com>
2021-07-28 17:33:13 +0700

    change the api resource

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const url =
-  "https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
+// const url =
+//   "https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
 
+const url =
+  "https://apicovid19indonesia-v2.vercel.app/api/indonesia/provinsi/more";
 const container = document.getElementById("page-container");
 const dataCovidContainer = document.getElementById("data-covid");
 const darkLight = document.getElementById("icon-dark-light");
@@ -12,19 +14,18 @@ const moonIcon = document.querySelector(".moon-icon");
 async function getDataCovid() {
   const response = await fetch(url);
   const datas = await response.json();
-  const features = datas.features;
+  // const features = datas.features;
+  console.log(datas);
 
-  features.forEach((data, i) => {
+  datas.forEach((data, i) => {
+    console.log(data);
     const box = document.createElement("div");
     box.setAttribute("class", "data-covid-content");
-    const attribut = data.attributes;
-
     // create p element for provinsi
     const provinsi = document.createElement("p");
     provinsi.setAttribute("class", "provinsi data-covid-provinsi");
-    provinsi.innerHTML = attribut.Provinsi;
+    provinsi.innerHTML = data.provinsi;
     box.appendChild(provinsi);
-
     // create box for data
     const boxDataSembuh = document.createElement("div");
     boxDataSembuh.setAttribute("class", "box-data-result");
@@ -32,14 +33,15 @@ async function getDataCovid() {
     boxDataPositif.setAttribute("class", "box-data-result");
     const boxDataMeninggal = document.createElement("div");
     boxDataMeninggal.setAttribute("class", "box-data-result");
-
+    const boxDatakasusUpdate = document.createElement("div");
+    boxDatakasusUpdate.setAttribute("class", "box-data-result");
     // create p element for Kasus_Semb
     const sembuh = document.createElement("p");
     sembuh.setAttribute("class", "sembuh result");
     const sembuhData = document.createElement("p");
     sembuhData.setAttribute("class", "sembuh-data");
     sembuh.innerHTML = "Sembuh ";
-    sembuhData.innerHTML = ": " + attribut.Kasus_Semb;
+    sembuhData.innerHTML = ": " + data.sembuh;
     boxDataSembuh.appendChild(sembuh);
     boxDataSembuh.appendChild(sembuhData);
     box.appendChild(boxDataSembuh);
@@ -49,7 +51,7 @@ async function getDataCovid() {
     const positifData = document.createElement("p");
     positifData.setAttribute("class", "positif-data");
     positif.innerHTML = "Positif ";
-    positifData.innerHTML = ": " + attribut.Kasus_Posi;
+    positifData.innerHTML = ": " + data.kasus;
     boxDataPositif.appendChild(positif);
     boxDataPositif.appendChild(positifData);
     box.appendChild(boxDataPositif);
@@ -59,10 +61,20 @@ async function getDataCovid() {
     const meninggalData = document.createElement("p");
     meninggalData.setAttribute("class", "meninggal-data");
     meninggal.innerHTML = "Meninggal ";
-    meninggalData.innerHTML = ": " + attribut.Kasus_Meni;
+    meninggalData.innerHTML = ": " + data.meninggal;
     boxDataMeninggal.appendChild(meninggal);
     boxDataMeninggal.appendChild(meninggalData);
     box.appendChild(boxDataMeninggal);
+    // latest update
+    const kasusUpdate = document.createElement("p");
+    kasusUpdate.setAttribute("class", "kasusUpdate result");
+    const kasusUpdateData = document.createElement("p");
+    kasusUpdateData.setAttribute("class", "kasus-update-data");
+    kasusUpdate.innerHTML = "Tanggal update ";
+    kasusUpdateData.innerHTML = ": " + data.last_date;
+    boxDatakasusUpdate.appendChild(kasusUpdate);
+    boxDatakasusUpdate.appendChild(kasusUpdateData);
+    box.appendChild(boxDatakasusUpdate);
 
     dataCovidContainer.appendChild(box);
   });
